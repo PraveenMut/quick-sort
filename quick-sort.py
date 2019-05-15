7733e44f31581b505fae9e9bac8958e43491528a
praveen <contact@praveenmuthu.dev>
2019-05-16 09:00:30 +1000

    adding comments

--- quick-sort.py
+++ quick-sort.py
@@ -3,6 +3,7 @@
 # tester array
 arr = [7,6,5,4,3,2,1,0]
 
+# partition (pivot) procedure
 def partition(arr, start, end): 
   pivot = arr[end]
   partitionIndex = start
@@ -17,13 +18,15 @@ def partition(arr, start, end):
   arr[end],arr[partitionIndex]  = arr[partitionIndex],arr[end]
   return partitionIndex
 
+# parent Quicksort algorithm
 def quickSort(arr, start, end):
   if (start < end):
     partitionIndex = partition(arr, start, end)
     quickSort(arr, start, partitionIndex-1)
     quickSort(arr, partitionIndex+1, end)
 
+## testers
 n = len(arr)
 quickSort(arr, 0, n-1)
-for i in range(n):
+for i in range(n): # print the elements in a new line
   print arr[i]
